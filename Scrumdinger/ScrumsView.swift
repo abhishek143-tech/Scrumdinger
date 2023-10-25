7c9b9b6d7b1926b510a68b7f736a324e3070fdd7
Abhishek Dhok <abhishekdhok96@gmail.com>
2023-10-25 12:16:53 +0530

    Initial Commit

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,36 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Abhishek Dilip Dhok on 24/10/23.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+
+    var body: some View {
+        NavigationStack {
+            List(scrums) { scrum in
+                NavigationLink(destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button {
+
+                } label: {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New scrum")
+            }
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
