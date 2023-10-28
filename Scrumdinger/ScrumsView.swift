199af235fca8d49c6daa0cacfd553b1dcb2268e8
Abhishek Dhok <abhishekdhok96@gmail.com>
2023-10-28 14:19:56 +0530

    - Adopted to the swift concurrency. - Persisting data. - Using this added logic to store the previous meeting records. - Added code to fetch and save data.

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct ScrumsView: View {
     @Binding var scrums: [DailyScrum]
+    @Environment(\.scenePhase) private var scenePhase
+    @State private var isPresentingNewScrumView = false
+    let saveAction: () -> Void
 
     var body: some View {
         NavigationStack {
@@ -21,16 +24,24 @@ struct ScrumsView: View {
             .navigationTitle("Daily Scrums")
             .toolbar {
                 Button {
-
+                    isPresentingNewScrumView = true
                 } label: {
                     Image(systemName: "plus")
                 }
                 .accessibilityLabel("New scrum")
             }
         }
+        .sheet(isPresented: $isPresentingNewScrumView) {
+            NewScrumSheet(scrums: $scrums, isPresentingNewScrumView: $isPresentingNewScrumView)
+        }
+        .onChange(of: scenePhase) { phase in
+            if phase == .inactive {
+                saveAction()
+            }
+        }
     }
 }
 
 #Preview {
-    ScrumsView(scrums: .constant(DailyScrum.sampleData))
+    ScrumsView(scrums: .constant(DailyScrum.sampleData), saveAction: {})
 }
